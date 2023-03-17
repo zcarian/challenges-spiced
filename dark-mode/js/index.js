5471e0ca4a3d9aa22a87422884cef24561d79220
zcarian <mar.jed.00@gmail.com>
2023-03-17 15:44:09 +0100

    completing the challeng

--- dark-mode/js/index.js
+++ dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
