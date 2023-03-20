d72de354d060fe4831ba04097c25193cbc41fef8
zcarian <mar.jed.00@gmail.com>
2023-03-20 12:44:08 +0100

    completing the challenge

--- teenager-check/js/index.js
+++ teenager-check/js/index.js
@@ -6,7 +6,11 @@ button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
   const age = Number(input.value);
-
+  if (age > 12 && age < 20) {
+    output.innerHTML = "You are a teen";
+  } else {
+    output.innerHTML = "You are not a teen.";
+  }
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
