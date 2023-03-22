99e7b9b2ff92b8e4d339bcb333ec481f751c18b8
zcarian <mar.jed.00@gmail.com>
2023-03-22 11:30:53 +0100

    completing the challenge

--- progress-bar/js/index.js
+++ progress-bar/js/index.js
@@ -21,4 +21,12 @@ For that:
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+  let progress = 0;
+  progress = window.scrollY / (document.body.clientHeight - window.innerHeight);
+  return String(progress * 100) + "%";
+}
+
+addEventListener("scroll", () => {
+  progressBar.style.width = calculateScrollPercentage();
+});
