2a8f94a4c3c3258a873d311c5c1c73eeef4b5ab9
zcarian <mar.jed.00@gmail.com>
2023-03-23 17:02:28 +0100

    completing the challenge

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -21,11 +21,27 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   let result;
 
   // --v-- write your code here --v--
+  formData = new FormData(event.target);
+  data = Object.fromEntries(formData);
 
+  const numberA = parseInt(data.numberA);
+  const numberB = parseInt(data.numberB);
+
+  if (data.operator === "addition") {
+    result = add(numberA, numberB);
+  } else if (data.operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (data.operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  } else if (data.operator === "division" && numberB !== 0) {
+    result = divide(numberA, numberB);
+  } else if (data.operator === "division" && numberB === 0) {
+    result = "🤬";
+    close();
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
