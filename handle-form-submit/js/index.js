5bc6ba7437b7038bf7f379660b808255e0b9fa22
zcarian <mar.jed.00@gmail.com>
2023-03-23 15:36:42 +0100

    copleting the challenge and BONUS

--- handle-form-submit/js/index.js
+++ handle-form-submit/js/index.js
@@ -1,3 +1,12 @@
-console.clear();
+// console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userData = new FormData(event.target);
+  const data = Object.fromEntries(userData);
+  console.log(data);
+  const sum = parseInt(data["age"]) + parseInt(data["badness"]);
+  console.log('The age-badness-sum of "firstName" is ', sum);
+});
