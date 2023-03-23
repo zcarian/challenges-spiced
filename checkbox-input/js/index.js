a83b03526716c39d32e9b0c4c855023821fc8888
zcarian <mar.jed.00@gmail.com>
2023-03-23 16:07:30 +0100

    completing the challenge and BONUS

--- checkbox-input/js/index.js
+++ checkbox-input/js/index.js
@@ -2,22 +2,34 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
+const success = document.querySelector('[data-js="success" ]');
 
-function hideTosError() {
-  tosError.setAttribute("hidden", "");
+function hideMessege(messege) {
+  messege.setAttribute("hidden", "");
 }
 
-function showTosError() {
-  tosError.removeAttribute("hidden");
+function showMessege(messege) {
+  messege.removeAttribute("hidden");
 }
+
+hideMessege(success);
+hideMessege(tosError);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  const elements = event.target.elements;
+  const ifCheckboxChecked = elements.tos.checked;
+  if (!ifCheckboxChecked) {
+    showMessege(tosError);
+    return;
+  } else {
+    showMessege(success);
+    hideMessege(tosError);
+    alert("Form submitted");
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
 });
