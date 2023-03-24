8fb86235f5eaa5ed0897d6c57cbc661fc253514c
zcarian <mar.jed.00@gmail.com>
2023-03-24 11:07:13 +0100

    completing the challenge

--- social-media-post2/js/index.js
+++ social-media-post2/js/index.js
@@ -10,3 +10,17 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+const newPostContent = "Lorem ipsum dolor, sit amet";
+const newUsername = "username2";
+newPost.innerHTML = `<p class="post__content">
+${newPostContent}</p>
+<footer class="post__footer">
+<span class="post__username">${newUsername}</span>
+<button type="button" class="post__button" data-js="like-button">
+  ♥ Like
+</button>
+</footer>`;
+document.body.append(newPost);
