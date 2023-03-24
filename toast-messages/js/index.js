3e0a46001c70eae13d947014f73ffedff0ebf1f6
zcarian <mar.jed.00@gmail.com>
2023-03-24 10:46:14 +0100

    completing the challenge

--- toast-messages/js/index.js
+++ toast-messages/js/index.js
@@ -6,8 +6,13 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  newToastMessage = document.createElement("li");
+  newToastMessage.classList.add("toast-container__message");
+  newToastMessage.innerText = "I'm another toast message";
+  toastContainer.append(newToastMessage);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
