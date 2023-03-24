f9cca783ebba2747264ced609bc0d29b0c443b5a
zcarian <mar.jed.00@gmail.com>
2023-03-24 16:06:42 +0100

    completing the challenge

--- simple-live-calculator/js/index.js
+++ simple-live-calculator/js/index.js
@@ -3,3 +3,13 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+firstInput.addEventListener("input", () => {
+  let multiplication = +firstInput.value * +secondInput.value;
+  result.textContent = multiplication;
+});
+
+secondInput.addEventListener("input", () => {
+  let multiplication = +firstInput.value * +secondInput.value;
+  result.textContent = multiplication;
+});
