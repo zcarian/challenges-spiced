809b0c0131df2953c2e3127a77d973da0c664932
zcarian <mar.jed.00@gmail.com>
2023-03-29 10:38:50 +0200

    second commit

--- user-login-callback-4-with-multiple-functions/js/index.js
+++ user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,8 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+function showErrorMessage(e) {}
