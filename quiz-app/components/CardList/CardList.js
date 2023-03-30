e1929f889d05ccdd1d3d8274f628a1d124e7f10d
zcarian <mar.jed.00@gmail.com>
2023-03-30 16:52:53 +0200

    completing the challenge

---
+++ quiz-app/components/CardList/CardList.js
@@ -0,0 +1,12 @@
+import { Card } from "../Card/Card.js";
+export function CardList(cards) {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
