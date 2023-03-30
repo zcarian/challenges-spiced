7124da49dd64e8171c73dff04182ef9fec181658
zcarian <mar.jed.00@gmail.com>
2023-03-30 16:04:13 +0200

    completing the challenge

--- spacerocket/js/launchSequence.js
+++ spacerocket/js/launchSequence.js
@@ -1,1 +1,19 @@
 // Implement the launch sequence function here and export it as the default export.
+import { loadPayload } from "./core/load.js";
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
+import { fuel } from "./core/fuel.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+
+export default function LaunchSequenceFunction() {
+  const countdownLength = 5;
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  fuel();
+  for (let i = 0; i < countdownLength; i++) {
+    countdown();
+  }
+  liftoff();
+  deployPayload();
+}
