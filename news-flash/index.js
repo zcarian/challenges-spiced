c8a6794f571090d735ea147f43e9ec54848ddb37
zcarian <mar.jed.00@gmail.com>
2023-04-01 15:31:04 +0200

    completing the challenge

--- news-flash/index.js
+++ news-flash/index.js
@@ -5,12 +5,13 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((card) =>
+  card.categories.includes("politics")
+);
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.sort((a, b) => a.body.length - b.body.length);
+// const sortedNews = filteredNews;
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
