1cf9835216efbde71c3aaaacf0614e47a09875de
zcarian <mar.jed.00@gmail.com>
2023-04-03 15:15:35 +0200

    completing the challenge

--- star-wars-console/js/index.js
+++ star-wars-console/js/index.js
@@ -2,6 +2,20 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error("Bad Response");
+    } else {
+      const starWarsData = await response.json();
+      starWarsData.results.forEach((character) => {
+        console.log(character);
+      });
+    }
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+}
 
 fetchData();
