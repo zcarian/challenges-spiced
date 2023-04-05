0f575957da88256ef487bbc8362e78fe364cf812
zcarian <mar.jed.00@gmail.com>
2023-04-05 11:00:47 +0200

    completing the challenge

--- rest-and-spread/index.js
+++ rest-and-spread/index.js
@@ -12,6 +12,8 @@ const person = {
   email: "jane@doe.com",
   phone: "12345",
 };
+const { age, ...contact } = person;
+// console.log(age, contact);
 
 // example: export const {value 1} = myObject;
 
@@ -20,6 +22,8 @@ const person = {
 // all other items should go into a variable called "restOfList".
 
 const list = ["one", "two", "three", "four", "five"];
+const [first, second, ...restOfList] = list;
+// console.log(first, second, restOfList);
 
 // EXERCISE 3
 // a) The `add` function does not work because it needs an array as argument.
@@ -27,11 +31,12 @@ const list = ["one", "two", "three", "four", "five"];
 // c) Make sure you understand what the rest syntax does here.
 // d) Note that the optional chaining `?.` is used to prevent an error to happen.
 
-function add(allNumbers) {
+function add(...allNumbers) {
   return allNumbers.reduce?.((a, b) => a + b);
 }
 
 export const sum = add(3, 2, 3, 2, 1, 2, 3, 4);
+// console.log(sum);
 
 // EXERCISE 4
 // a) Below, the `add` function is called with an array as argument.
@@ -39,7 +44,8 @@ export const sum = add(3, 2, 3, 2, 1, 2, 3, 4);
 // c) Make sure you understand what the spread syntax does here.
 
 const numbers = [3, 2, 1];
-export const result = add(numbers);
+export const result = add(...numbers);
+// console.log(result);
 
 // EXERCISE 5
 // a) Create a variable `allFruits` which contains `anotherFruit` and `fruits`
@@ -49,3 +55,5 @@ export const result = add(numbers);
 
 const fruits = ["apple", "banana", "orange", "papaya"];
 const anotherFruit = "cherry";
+const allFruits = [anotherFruit, ...fruits];
+console.log(allFruits);
