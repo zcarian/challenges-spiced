d8686e7f568f462595086a93113fceb8aca8f983
zcarian <mar.jed.00@gmail.com>
2023-04-11 15:27:45 +0200

    completing the challenge

--- hello-world-article/src/App.js
+++ hello-world-article/src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>I am a heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero adipisci
+        iste consequatur quam maiores vel fugiat cupiditate eos facere
+      </p>
+      <p>
+        illo numquam repellat voluptates sequi ab repudiandae nostrum sunt,
+        laborum laudantium.
+      </p>
+    </article>
+  );
 }
