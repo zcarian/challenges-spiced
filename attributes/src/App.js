75e40a6eb8d3bc6ff9719c5866956d3a98768287
zcarian <mar.jed.00@gmail.com>
2023-04-11 15:53:54 +0200

    completing the challenge

--- attributes/src/App.js
+++ attributes/src/App.js
@@ -2,5 +2,24 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">This is an Article</h2>
+      <label htmlFor="color">What is your favourite color</label>
+      <input id="color" type="color" name="color" />
+      <br />
+      <br />
+      <br />
+      <a
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        className="article__link"
+      >
+        Click me for free bitcoin
+      </a>
+    </article>
+  );
 }
