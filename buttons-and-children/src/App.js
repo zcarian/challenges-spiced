682501eb678c0ece7ffc61ec909fdf1b8552aca0
zcarian <mar.jed.00@gmail.com>
2023-04-12 15:08:45 +0200

    completing the challenge

--- buttons-and-children/src/App.js
+++ buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>I am a Button!</Button>
+      <Button>Me too!</Button>
+      <Button>So am I</Button>
+      <Button>Thera are 4 of us!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
