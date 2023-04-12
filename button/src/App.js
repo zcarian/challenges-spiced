8d5c20a09d56720d9e27fca680b338e6a77197bc
zcarian <mar.jed.00@gmail.com>
2023-04-12 11:36:37 +0200

    completing the challenge

--- button/src/App.js
+++ button/src/App.js
@@ -2,5 +2,44 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  return (
+    <div>
+      <Button
+        color="black"
+        text="Hello"
+        disabled={true}
+        onClick={handleClick}
+      />
+      <br />
+      <Button
+        color="orange"
+        text="Hello 2"
+        disabled={false}
+        onClick={handleClick}
+      />
+      <br />
+      <Button
+        color="blue"
+        text="Hello 3"
+        disabled={true}
+        onClick={handleClick}
+      />
+    </div>
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
