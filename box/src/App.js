92ca0e00780f63892e41979f5878e7df436b9099
zcarian <mar.jed.00@gmail.com>
2023-04-13 15:10:03 +0200

    completing the challenge

--- box/src/App.js
+++ box/src/App.js
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let isActive = false;
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
     // Check that the value changes correctly.
-    console.log(isActive);
+    console.clear();
+    console.log("This is inside", isActive);
   }
 
+  console.log("This is outside", isActive);
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Activate" : "Deactivate"}
+      </button>
     </main>
   );
 }
