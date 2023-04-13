ff2d5a54c812bd3100d6cc5fc40efec6d920dae0
zcarian <mar.jed.00@gmail.com>
2023-04-13 15:17:21 +0200

    completing the challenge

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -11,7 +12,7 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count - 1);
           }}
         >
           -
@@ -19,7 +20,7 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count + 1);
           }}
         >
           +
