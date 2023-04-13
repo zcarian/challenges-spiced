27a9092f5add2e9b6f58e50ac605738d94bb647a
zcarian <mar.jed.00@gmail.com>
2023-04-13 16:03:56 +0200

    completing the challenge

--- emoji-checker/src/App.js
+++ emoji-checker/src/App.js
@@ -1,52 +1,37 @@
+import { useState } from "react";
 import React from "react";
 import "./styles.css";
 
 export default function App() {
-  let code = "?";
+  const [code, setCode] = useState("?");
 
   const validCode = "🐡🐠🐋";
+
+  function handleClick(emoji) {
+    code === "?" ? setCode(emoji) : setCode(code + emoji);
+  }
 
   return (
     <div className="container">
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={() => handleClick("🐡")}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={() => handleClick("🐋")}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={() => handleClick("🐠")}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
         </button>{" "}
       </div>
 
-      <button
-        type="button"
-        onClick={() => {
-          console.log("Reset Code!");
-        }}
-      >
+      <button type="button" onClick={() => setCode("")}>
         Reset
       </button>
       <h2>{code}</h2>
