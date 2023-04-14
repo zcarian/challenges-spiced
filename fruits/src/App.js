36ab341fc2373288555d5b2b7002641009fddb7c
zcarian <mar.jed.00@gmail.com>
2023-04-14 10:32:56 +0200

    completing the challenge

--- fruits/src/App.js
+++ fruits/src/App.js
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1,
+      name: "🍎 Red apple",
+      color: "red",
+    },
+    {
+      id: 2,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 3,
+      name: "🍐 Pear",
+      color: "green",
+    },
+    {
+      id: 4,
+      name: "🍋 Lemon",
+      color: "yellow",
+    },
+    {
+      id: 5,
+      name: "🍏 Green apple",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit, index) => {
+        return <Card key={index} fruit={fruit} />;
+      })}
     </div>
   );
 }
--- fruits/src/components/Card.js
+++ fruits/src/components/Card.js
@@ -1,5 +1,5 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ fruit }) {
+  return <p className={`card card--${fruit.color}`}>{fruit.name}</p>;
 }
