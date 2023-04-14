df797e082b261c7e8bd21842a5459abbaca1c243
zcarian <mar.jed.00@gmail.com>
2023-04-14 11:28:24 +0200

    completing the challenge

--- journal-app-entries-array/src/components/Entry/index.js
+++ journal-app-entries-array/src/components/Entry/index.js
@@ -1,18 +1,18 @@
 import FavoriteButton from "../FavoriteButton";
 import "./Entry.css";
 
-export default function Entry({ motto, notes, date }) {
+export default function Entry({ entry }) {
   return (
     <article className="entry">
-      <time className="entry__date">{date}</time>
+      <time className="entry__date">{entry.date}</time>
       <div className="entry__content">
         <div className="entry__motto-container">
           <h2 className="entry__motto">
-            <q>{motto}</q>
+            <q>{entry.motto}</q>
           </h2>
           <FavoriteButton />
         </div>
-        <p className="entry__notes">{notes}</p>
+        <p className="entry__notes">{entry.notes}</p>
       </div>
     </article>
   );
