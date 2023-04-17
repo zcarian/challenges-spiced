87de2f4ae844c79f7b67778088d54c5b4d9be67c
zcarian <mar.jed.00@gmail.com>
2023-04-17 18:39:38 +0200

    completing the challenge

--- api-status-2/src/components/StatusCheck/index.js
+++ api-status-2/src/components/StatusCheck/index.js
@@ -1,12 +1,13 @@
 import "./StatusCheck.css";
+import { useState } from "react";
 
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
-  const statusIcon = "⁉️";
+  const [statusIcon, setStatusIcon] = useState("⏳");
   // Something needs to change here…
   // ↙️
-  function handleCheckApiStatus() {
+  async function handleCheckApiStatus() {
     /**
      * Hint 1:
      * Use the `fetch()` function and pass the `apiStatusUrl` into it
@@ -20,6 +21,20 @@ export default function StatusCheck() {
      * is okay and false if it is not.
      **/
     // --v-- write your code here --v--
+    setStatusIcon("⏳");
+    try {
+      const response = await fetch(apiStatusUrl);
+      // const data = await response.json();
+      console.log(response);
+      if (response.ok) {
+        setStatusIcon("✅");
+      } else {
+        setStatusIcon("❌");
+      }
+    } catch (error) {
+      console.log(error);
+      setStatusIcon("🚨");
+    }
     // --^-- write your code here --^--
   }
 
