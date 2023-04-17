b9e3a4a77fa04235858d484bc4d162e0cfea7d97
zcarian <mar.jed.00@gmail.com>
2023-04-17 11:53:40 +0200

    completing the challenge

--- adding-animals/src/App.js
+++ adding-animals/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { uid } from "uid";
 import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
@@ -26,6 +27,7 @@ export default function App() {
 
   function handleAddAnimal(newAnimal) {
     console.log(newAnimal);
+    setAnimals([...animals, { id: uid(), ...newAnimal }]);
   }
 
   return (
