d8ec0031472e23c8a5848ccc7c906814b0fc8b5f
zcarian <mar.jed.00@gmail.com>
2023-04-17 13:09:34 +0200

    completing Favorite Button and Shared State

--- journal-app-form/src/components/FavoriteButton/index.js
+++ journal-app-form/src/components/FavoriteButton/index.js
@@ -1,17 +1,12 @@
-import { useState } from "react";
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
 
-export default function FavoriteButton() {
-  const [isFavorite, setIsFavorite] = useState(false);
-
+export default function FavoriteButton({ id, isFavorite, onToggleFavorite }) {
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        setIsFavorite(!isFavorite);
-      }}
+      onClick={() => onToggleFavorite(id)}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
