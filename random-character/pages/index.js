3346ca56051724c8a18308844107d993284d3ae4
zcarian <mar.jed.00@gmail.com>
2023-05-04 16:49:59 +0200

    completing the challenge

--- random-character/pages/index.js
+++ random-character/pages/index.js
@@ -1,3 +1,16 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data } = useSWR("/api/random-character", fetcher);
+  return (
+    <div>
+      <h1>Random Character</h1>
+      <p>First Name: {data?.firstName}</p>
+      <p>Last Name: {data?.lastName}</p>
+      <p>Age: {data?.age}</p>
+      <p>Email: {data?.email}</p>
+    </div>
+  )
 }
