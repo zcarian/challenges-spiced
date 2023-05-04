60448b9bfca0957078c26d921b057e2bb78765c3
zcarian <mar.jed.00@gmail.com>
2023-05-04 10:50:50 +0200

    completing the challenge

--- chance/server.js
+++ chance/server.js
@@ -1,3 +1,11 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((req, res) => {
+    const name = chance.name();
+    const age = chance.age();
+    const profession = chance.profession();
+    res.end(`Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`)
+});
